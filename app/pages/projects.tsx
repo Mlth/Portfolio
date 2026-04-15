f67a1aea7073c0728e31ba0ccd38bed8d9ca0da7
Mlth <mlth@itu.dk>
2026-04-15 11:47:57 +0200

    Replaced gifs with videos for improved resolution, added info to almenr card

--- app/pages/projects.tsx
+++ app/pages/projects.tsx
@@ -1,12 +1,16 @@
 import PageBlock from "~/components/pageBlock";
 import { almenrGreen, almenrOffGreen, almenrOffOrange, almenrOffWhite, almenrOrange, type ColorPack } from "~/utility/colorUtils";
-import almenr from '~/resources/almenr.gif'
+import almenr from '~/resources/almenr.mp4'
+import gammalstorp from '~/resources/gammalstorp.mp4'
+import type { JSX } from "react";
+import { Link } from "react-router";
 
 type Project = {
     colors: ColorPack;
-    image: string;
+    media: string; 
+    mediaIsVideo?: boolean;
     title: string;
-    description: string;
+    description: JSX.Element;
     link: string;
 }
 
@@ -18,9 +22,27 @@ export default function Projects() {
                 middleground: almenrOrange,
                 background: almenrOffOrange,
             },
-            description: 'THIS IS A DESCRIPTION',
+            description: (
+                <>
+                    <h3>What is Almenr?</h3>
+                    <p className="italic">Almenr facilitates, builds, and maintains co-living communites all over Denmark</p>
+                    <p>To ease the experience of these three processes, Almenr, and their customers, use an online platform to facilitate communication, events, registrations, and much more!</p>
+                    <div className="flex justify-center py-2">
+                        <Link 
+                            to='https://almenr.dk/u' 
+                            className="underline hover:no-underline italic"
+                            target="_blank" rel="noopener noreferrer"
+                        >
+                                <h4>Check it out!</h4>
+                        </Link>
+                    </div>
+                    <br/>
+                    <h3>My contribution!</h3>
+                </>
+            ),
             title: 'Almenr online platform',
-            image: './../resources/almenr.gif',
+            media: almenr,
+            mediaIsVideo: true,
             link: 'https://almenr.dk/u'
         },
         {
@@ -29,9 +51,15 @@ export default function Projects() {
                 middleground: almenrGreen,
                 background: almenrOffGreen,
             },
-            description: 'Gammalstorp is a cool fritidslandsby',
-            title: 'Gammalstorp booking system',
-            image: 'nop :(',
+            description: (
+                <>
+                    <h3>What is Gammalstorp?</h3>
+                    <p>Gammalstorp is a cool fritidslandsby</p>
+                </>
+            ),
+            title: 'Gammalstorp booking',
+            media: gammalstorp,
+            mediaIsVideo: true,
             link: 'https://almenr.dk/gammalstorp'
         }
     ]
@@ -40,21 +68,32 @@ export default function Projects() {
     return projects.map((p, i) => {
         const textLeft = i % 2 === 0;
 
-        const imageElement = (
+        const mediaElement = (
             <div className="overflow-hidden flex justify-center">
-                <img 
-                    key={almenr} src={almenr} alt="loading..." 
-                    className={`block shrink w-auto h-auto
-                        ${textLeft ? 'rounded-r-4xl' : 'rounded-l-4xl'}    
-                    `} 
-                />
+                {p.mediaIsVideo
+                    ? ( 
+                        <video
+                            key={i} src={p.media} autoPlay loop muted
+                            className={`object-cover
+                                ${textLeft ? 'rounded-r-4xl' : 'rounded-l-4xl'}    
+                            `}
+                        />
+                    ) : (
+                        <img 
+                            key={i} src={p.media} alt="loading..." 
+                            className={`object-cover
+                                ${textLeft ? 'rounded-r-4xl' : 'rounded-l-4xl'}    
+                            `} 
+                        />
+                    )
+                }
             </div>
             
         )
         const textElement = (
             <div className="p-5">
-                <h1 className="font-bungee text-4xl">{p.title}</h1>
-                <p className="mt-1">{p.description}</p>
+                <h1 className="pt-2">{p.title}</h1>
+                <p className="px-5 py-4">{p.description}</p>
             </div>
         )
 
@@ -63,17 +102,17 @@ export default function Projects() {
             key={p.title}
         >
             <div
-                className="w-full min-h-full rounded-4xl grid grid-cols-2"
+                className="w-full min-h-full rounded-4xl grid grid-cols-2 text-center"
                 style={{ backgroundColor: p.colors.middleground, color: p.colors.foreground }}
             >
                 {textLeft ? (
                     <>
                         {textElement}
-                        {imageElement}
+                        {mediaElement}
                     </>
                 ) : (
                     <>
-                        {imageElement}
+                        {mediaElement}
                         {textElement}
                     </>
                 )}
